8489dc403a60fd4eae8abaf9055beb3c8e5aa1bb
Adam Dusty <adam.dusty27@gmail.com>
2024-02-26 19:50:27 -0700

    Updated compile options

--- cmake/compile-options.cmake
+++ cmake/compile-options.cmake
@@ -1,8 +1,8 @@
 function(add_dev_compile_opts target)
     target_compile_options(${target} PRIVATE
         $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>,$<CXX_COMPILER_ID:GNU>>:
-            -U_FORTIFY_SOURCE
-            -D_FORTIFY_SOURCE=3
+            $<$<NOT:$<CONFIG:debug>>:-U_FORTIFY_SOURCE>
+            $<$<NOT:$<CONFIG:debug>>:-D_FORTIFY_SOURCE=3>
             -fstack-protector-strong
             -fcf-protection=full
             -fstack-clash-protection
